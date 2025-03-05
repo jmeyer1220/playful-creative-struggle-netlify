6654226bd1781c527add8a96e5a445abb7679f8b
downtothemeyer <jmeyer1220@gmail.com>
2025-03-05 12:14:40 -0600

    Updating Netlify Build

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,14 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 }));
